05c18f92a357fd30e8d99a180de2e84921fbdde9
Goddartable <goddart777@gmail.com>
2023-09-25 20:38:23 +0300

    gitignore, build.gradle, TestBase

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -1,2 +1,11 @@
-package guru.qa;public class TestBase {
+package guru.qa;
+
+import org.junit.jupiter.api.BeforeAll;
+import io.restassured.RestAssured;
+
+public class TestBase {
+    @BeforeAll
+    static void settingURI() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
 }
