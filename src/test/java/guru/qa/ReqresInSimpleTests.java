f21d243a40aa4160400fe1a4e516f13147e02bdb
Goddartable <goddart777@gmail.com>
2023-09-27 21:27:40 +0300

    Final

--- src/test/java/guru/qa/ReqresInSimpleTests.java
+++ src/test/java/guru/qa/ReqresInSimpleTests.java
@@ -9,6 +9,62 @@ import static org.hamcrest.Matchers.*;
 
 public class ReqresInSimpleTests extends TestBase{
     @Test
+    @DisplayName("CREATE")
+    void createUser(){
+        given().
+                contentType(JSON)
+                .body(randomUtils.jsonBodyCreate.toString())
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo(randomUtils.userName)
+                        , "job", equalTo(randomUtils.userJob)
+                        , "id", notNullValue()
+                        , "createdAt", greaterThan(randomUtils.timeBeforeStartTest));
+    }
+    @Test
+    @DisplayName("REGISTER - SUCCESSFUL")
+    void registerSuccess(){
+        given().
+                contentType(JSON)
+                .when()
+                .body(randomUtils.jsonBodyRegister.toString())
+                .post("/api/register")
+                .then()
+                .statusCode(200)
+                .body("id",notNullValue()
+                , "token", notNullValue());
+    }
+
+    @Test
+    @DisplayName("REGISTER - UNSUCCESSFUL")
+    void registerUnSuccess(){
+        given().
+                contentType(JSON)
+                .when()
+                .body(randomUtils.jsonBodyRegisterFail.toString())
+                .post("/api/register")
+                .then()
+                .statusCode(400)
+                .body("error",equalTo("Note: Only defined users succeed registration"));
+    }
+
+    @Test
+    @DisplayName("LOGIN - SUCCESSFUL")
+    void loginSuccess(){
+        given().
+                contentType(JSON)
+                .body(randomUtils.jsonBodyRegister.toString())
+                .when()
+                .post("/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", equalTo("QpwL5tke4Pnpja7X4"));
+
+    }
+
+    @Test
     @DisplayName("LOGIN - UNSUCCESSFUL")
     void loginFailed(){
         given().
